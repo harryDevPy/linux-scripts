aa28850191c31d51bdf3809b3fb21bacaa7e99a4
harryDevPy <30123559+harryDevPy@users.noreply.github.com>
2020-09-23 13:00:47 +0200

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -2,7 +2,7 @@
 echo "#!/bin/bash" > ~/update.sh
 echo 'echo "running update..."' >> ~/update.sh
 echo '# intenzionale 2 volte "apt-get autoremove -y"' >> ~/update.sh
-echo "dpkg --configure -a && apt-get install -f && apt-get --purge autoremove && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y && apt-get autoclean -y" >> ~/update.sh
+echo "dpkg --configure -a && apt-get install -f && apt-get --purge autoremove -y && apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get autoremove -y && apt-get autoclean -y" >> ~/update.sh
 echo 'echo "end update."' >> ~/update.sh
 sudo mv ~/update.sh /usr/bin
 sudo chmod +x /usr/bin/update.sh
@@ -20,6 +20,14 @@ sudo gawk -i inplace '!a[$0]++' ~/.bash_aliases
 echo "==============================================="
 echo "RICORDA:  . ~/.bash_aliases"
 echo 'alias "up" già pronto e aggiunto a sudoers'
+
+
+
+
+
+
+
+
 
 
 
