07125da2142eed1607dd73ef51a9ea409a77026e
harryDevPy <30123559+harryDevPy@users.noreply.github.com>
2020-10-14 13:41:27 +0200

    Update vbox.sh

--- vbox.sh
+++ vbox.sh
@@ -1,5 +1,5 @@
 
-echo "NON USARE SUDO - solo: sh vbox.sh"
+echo "\nNON USARE SUDO - solo: sh vbox.sh\n"
 u="$USER"
 sudo cp /media/$u/VBox_GAs*/VBoxLinuxAdditions.run ~/
 sudo sh /home/$u/VBoxLinuxAdditions.run
