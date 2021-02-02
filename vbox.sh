cc5f966b3efc102a01765b3e0dac8863be468f56
harryDevPy <30123559+harryDevPy@users.noreply.github.com>
2021-02-02 07:04:54 +0100

    Update vbox.sh

--- vbox.sh
+++ vbox.sh
@@ -1,6 +1,6 @@
 
 echo "\nNON USARE SUDO - solo: sh vbox.sh\n"
 echo "\nSE NON FUNZIONA, FAI CLICK SUL VBox_GAs* \n"
-u="$USER"
-sudo sh /media/$u/VBox_GAs*/VBoxLinuxAdditions.run
+
+sudo sh /media/$USER/VBox_GAs*/VBoxLinuxAdditions.run
 
